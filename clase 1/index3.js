8f4f4d20e171b0bb9485d2afc623160309810abf
Diego Rodriguez Pinedo <diegorodriguezpinedo@gmail.com>
2022-04-14 18:02:13 -0300

    initial commit

---
+++ clase 1/index3.js
@@ -0,0 +1,39 @@
+// let nameUser = prompt ("¿Cual es tu nombre?");
+// let surnameUser = prompt ("¿Cual es tu apellido?");
+// console.log("Bienvenido a consola" + " " + nameUser + " " + surnameUser );
+// alert ("Hola" + " " + nameUser + " " + surnameUser + "" + "!");
+
+//concatenación
+// let nameUser = prompt ("¿Cual es tu nombre?");
+// let surnameUser = prompt ("¿Cual es tu apellido?");
+// let edad = prompt ("¿Cual es tu edad?");
+// let resultado = (nameUser + " " + surnameUser + " " + "tienes" + " " + edad + " " + "años");
+
+// alert ("Hola" + " " + resultado);
+// console.log ("Bienvenido a consola" + " " + resultado);
+
+// ----------------------------------------------------------------
+// Que edad tienes?
+// let birthday = prompt ("¿En que año naciste?");
+// let actualYear = prompt ("¿En que año estamos actualmente?");
+// let result = (parseFloat(actualYear) - parseFloat(birthday))
+
+// console.log("Tienes aproximadamente" + " " + result + " " + "años" );
+// alert ("Tienes aproximadamente" + " " + result + " " + "años" );
+
+// -------------------------------------------------------------
+// Calcular cuantos días llevas vividos
+// let naciste = prompt ("¿En que año naciste?");
+// let diasVividos = naciste * 365;
+// console.log("Aproximadamente llevas" + " " + diasVividos + " " + "días" + " " + "vividos." );
+// alert ("Viviste aproximadamente" + " " + diasVividos + " " + "días" );
+
+// -------------------------------------------------------------
+// Calcular cuantos días llevas vividos exactamente
+// let naciste = prompt ("¿En que fecha naciste?");
+
+// -------------------------------------------------------------
+let naciste = 30;
+let diasVividos = "20";
+let resultado = naciste + diasVividos;
+console.log(resultado);
---
+++ clase2/index.js
@@ -0,0 +1,36 @@
+// console.log("3" == tres);
+
+// console.log("3" === 3);
+// let nombre = prompt ("");
+
+// if (condition) {
+    
+// }else if (condition) {
+    
+// } else {
+//     alert ("dejaste un campo vacio!");
+// }
+
+
+// if(nombre != ""){
+//     if(nombre === "andres"){
+//       alert("ingresaste andres");
+//     }else if(nombre === "camilo"){
+//       alert("ingresaste camilo");
+//     }else{
+//       alert("no ingresaste ninngun nombre valido");
+//     }
+//   }
+
+
+
+let numerito = prompt("ingrese un número");
+
+if ((numerito === 100) || (numerito < 101)){
+    alert("el numero es igual a 100!");
+}else {
+    alert("rayos, el numero es mayor a 100!");
+}
+
+console.log("buen trabajo!");
+
