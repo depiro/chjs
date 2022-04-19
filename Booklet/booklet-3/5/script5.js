a910df69893ae445599c7334940e6e005da7901d
Diego Rodriguez Pinedo <diegorodriguezpinedo@gmail.com>
2022-04-19 17:35:48 -0300

    desafio 1 - clase 3

--- Booklet/booklet-3/5/script5.js
+++ Booklet/booklet-3/5/script5.js
@@ -1,19 +1,31 @@
-// Actividad 5: comprando productos
-let numero = prompt("ingresar personajes de harry potter");
-let ingresados = '';
+// // Actividad 5: comprando productos
 
-while (personajes != 'ESC') {
-    ingresados += personajes +"\n";
-    personajes = prompt("ingresar personajes de harry potter");
+let entrada = '';
+while (entrada != 'ESC') {
+    switch (entrada) {
+        case "1":
+            alert(entrada + ' ' + "tomate");
+            break;
+        case "2":
+            alert(entrada + ' ' + "papa");
+            break;
+        case "3":
+            alert(entrada + ' ' + "carne");
+            break;
+        case "4":
+            alert(entrada + ' ' + "pollo");
+            break;
+        case "5":
+            alert("E R R O R , no hay producto con ese numero");
+            break;
+        default:
+            alert("H O L A! este es el inicio");
+            break;
+    }
+    entrada = prompt("elegir productos ingresando numeros de 1 a 4");
 }
-alert(ingresados);
 
 
-// let entrada    = prompt("INGRESAR NOMBRE");
-// let ingresados = '';
-// while (entrada != 'Voldemort') {
-//     ingresados += entrada +"\n";
-//     entrada     = prompt("INGRESAR NOMBRE");
-// }
-// alert(ingresados);
-
+function name(params) {
+    functio
+}
