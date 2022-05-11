e9d9d01d696cb9fed031c3c90638a97f2d5d2a03
Diego Rodriguez Pinedo <diegorodriguezpinedo@gmail.com>
2022-05-10 23:40:24 -0300

    desafío complementario Interactuar con HTML - DOM

--- proyecto-final/scrip.js
+++ proyecto-final/scrip.js
@@ -1,79 +1,125 @@
-class Guitar {
-    constructor (brand,model,price) {
-        this.brand = brand;
-        this.model = model;
-        this.price = price;
-    }
+const contenedor = document.getElementById("productos");
+const tablaCarrito = document.getElementById("tablaCarrito");
+const carrito = [];
 
-    toString() {
-        return `${this.brand}. ${this.model}, ${this.price} USD`;
+
+const PRODUCTOS = [
+    {
+        id: 1,
+        brand: "Gibson",
+        model: `Marauder Custom 1976`,
+        price: 15000,
+        image: `https://www.springinstrumentos.com/images/productos/197/fotos/max_5660f07f95bdc9963ea788d2fe335ea4.jpg`
+    },
+    {
+        id: 2,
+        brand: "Gibson",
+        model: `SG Standard '61 2020`,
+        price: 5000,
+        image: `https://www.springinstrumentos.com/images/productos/300/fotos/max_f56f524170abe6f61a23fad48a795fa3.jpg`
+    },
+    {
+        id: 3,
+        brand: "Gibson",
+        model: `BR-9 Lapsteel 1950`,
+        price: 1190,
+        image: `https://www.springinstrumentos.com/images/productos/264/fotos/max_5857148defba089eb722c9353a1e9fa6.jpg?v=1638555537`
+    },
+    {
+        id: 4,
+        brand: `Fender`, 
+        model: `Telecaster Custom Shop Relic '64 2009`, 
+        price: 4490, 
+        image: `https://www.springinstrumentos.com/images/productos/277/fotos/max_5518e1c8aa2630cd643832f5ccd8d2e7.jpg?v=1645023773`
+    },
+    {
+        id: 4,
+        brand: `Fender`, 
+        model: `Telecaster Custom Shop 2006 Ri 1963 Sea Foam`, 
+        price: 3490, 
+        image: `https://www.springinstrumentos.com/images/productos/307/fotos/max_0733e52d13dfd1702c0a24edafe28acc.jpg?v=1650563790`
+    },
+    {
+        id: 4,
+        brand: `Fender`, 
+        model: `Telecaster Custom Shop 2006 Ri 1963 Sea Foam`, 
+        price: 3490, 
+        image: `https://www.springinstrumentos.com/images/productos/304/fotos/max_a59ae22103c0234b6c547ef868cad0b5.jpg?v=1650473417`
+    },
+    {
+        id: 4,
+        brand: `Fender`, 
+        model: `Telecaster Custom Shop 2006 Ri 1963 Sea Foam`, 
+        price: 3490, 
+        image: `https://www.springinstrumentos.com/images/productos/178/fotos/max_ad065a3537874b099c6dffc9bc3dbdad.jpg?v=1620185534`
     }
+];
+
+
+const getCard = (item) => {
+    return (
+        `
+        <div class="col">
+            <div class="card">
+                <img src="${item.image}" class="card-img-top" alt="${item.brand}">
+                <div class="card-body">
+                    <h5 class="card-title">${item.brand}</h5>
+                    <p class="card-text">${item.model}</p> 
+                    <p class="card-text">U$S ${item.price}</p> 
+                    <button onclick=agregarCarrito(${item.id}) class="btn btn-primary">Agregar al carrito</button>
+                </div>
+            </div>
+        </div>
+    `);
+};
+
+const getRow = (item) => {
+    return(
+        `
+    <tr>
+        <th scope="row">${item.id}</th>
+        <td>${item.brand}</td>
+        <td>${item.model}</td>
+        <td>$${item.price}</td>
+        <td><img style="width:20px" class="pic-table" src="${item.image}" alt="imagen"></td>
+    </tr>
+        `
+    )
 }
 
-// Agrega una guitarra a la lista de venta
-function newGuitar(brand, model, price) {
-    stockGuitars.unshift(new Guitar(brand, model, price));
-}
-const stockGuitars = []
-newGuitar(`Fender`, `Stratocaster Custom Shop 60's Relic`, 4490);
-newGuitar(`Fender`, `Telecaster AVRI '52 Butterscotch 2016`, 2290);
-newGuitar(`Gibson`, `SG Standard '61`, 4990);
-newGuitar(`Gibson`, `Les Paul 25/50 Tobacco Burst 1978`, 3490);
+// función flecha
+const cargarProductos = (datos, nodo, Tabla) => {
+    let acumulador = "";
+    datos.forEach((el) => {
+        acumulador += Tabla ? getRow(el) : getCard(el);
+    })
+    nodo.innerHTML = acumulador;
+};
 
-// Menu de navegción
-function navMenu() {
-    const selectItem = prompt(
-        `Bienvenido! compramos y vendemos guitarras \n Selecciona una opción ingresando un número: \n
-        1- Comprar \n
-        2- Vender \n
-        3- Salir \n`);
-
-    switch(selectItem){
-            case `1`:
-                function saleGuitars() {
-                    let listStockGuitars = '';
-                    
-                    for(const guitar of stockGuitars) {
-                        let seeGuitar = `${stockGuitars.indexOf(guitar) + 1} - ${guitar.brand}, (${guitar.model}), ${guitar.price} USD\n`;
-                        
-                        listStockGuitars += seeGuitar;
-                    }
-                    return listStockGuitars;
-                }
-
-                let selectGuitar = parseInt(prompt(`Selecciona la que te gustaría comprar ingresando el número correspondiente: \n ${saleGuitars()}`));
-
-                let indexGuitar = selectGuitar - 1;
-
-                alert(`Compraste ${stockGuitars[indexGuitar].brand} ${stockGuitars[indexGuitar].model} ${stockGuitars[indexGuitar].price} USD\n Te llamaremos para finalizar la compra.`);
-
-                stockGuitars.splice(indexGuitar, 1);
-                navMenu();
-                break;
-
-            case `2`:
-                alert('Para iniciar la venta, describenos tu instrumento.');
+const agregarCarrito = (id) => {
+    const seleccion = PRODUCTOS.find(item => item.id === id);
+    const busqueda = carrito.findIndex(el => el.id === id);
     
-                let brandGuitar = prompt('¿Que marca es tu guitarra?');
-                let modelGuitar = prompt('¿Que modelo?');
-                let priceGuitar = prompt('¿Cual es precio?');
- 
-                newGuitar(brandGuitar,modelGuitar,priceGuitar);
-    
-                alert(`Ya está en venta tu guitarra.\nPodes verla seleccionando la opcion COMPRAR.\n 
-                ${stockGuitars.join("\n")}`);
-                
-                navMenu();
-                break;
-
-            case `3`:
-                break;
-            
-            default:
-                navMenu();
-                break;
-
+    if (busqueda === -1) {
+        carrito.push({
+            id: seleccion.id,
+            brand: seleccion.brand,
+            price: seleccion.price,
+            image: seleccion.image,
+        })
+    } else {
+        carrito[busqueda].amount = carrito[busqueda].amount + 1
     }
+    
+    cargarProductos(carrito, tablaCarrito, true);
 }
-navMenu()
+cargarProductos(PRODUCTOS, contenedor, false);
 
+let brandGuitar = document.getElementById("brand-guitar");
+let modelGuitar = document.getElementById("model-guitar");
+let priceGuitar = document.getElementById("price-guitar");
+
+
+let usuario = prompt("¡Bienvenido! ¿Cuál es tu nombre?");
+let welcome = document.getElementById("welcome");
+welcome.innerHTML = `<h1 class="fw-light">¡Bienvenido, ${usuario}!</h1><br><p class="lead text-muted">Vendemos y compramos guitarras</p>`;
